08d73c8e7870cc2acb1a1b5e58771b10687bb797
JJINNN2 <gyswlsl123@sookmyung.ac.kr>
2021-09-11 23:38:25 +0900

    change intent page

--- app/src/main/java/com/example/letsbasket/chattingTab/ChattingAdapter.kt
+++ app/src/main/java/com/example/letsbasket/chattingTab/ChattingAdapter.kt
@@ -10,6 +10,7 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.letsbasket.R
+import com.example.letsbasket.chattingTab.ChattingMsgActivity
 import com.example.letsbasket.itemDetail.ItemActivity
 
 class ChattingAdapter(private val context: Context): RecyclerView.Adapter<ChattingAdapter.ViewHolder>() {
@@ -41,12 +42,10 @@ class ChattingAdapter(private val context: Context): RecyclerView.Adapter<Chatti
 
             // 클릭 이벤트 추가
             itemView.setOnClickListener{
-                Intent(context, ItemActivity::class.java).apply {
-                    putExtra("postID", item.postID)
+                Intent(context, ChattingMsgActivity::class.java).apply {
                     addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
                 }.run { context.startActivity(this) }
             }
         }
-
     }
 }
